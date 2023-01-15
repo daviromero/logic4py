2c2a5aec7e4cccb0f67f483e94428ac1fc1bec4a
Davi Romero de Vasconcelos <daviromero@ufc.br>
2023-01-15 18:26:29 -0300

    ajustes

--- src/logic4py/formula.py
+++ src/logic4py/formula.py
@@ -633,6 +633,7 @@ def consequence_logic(formula, premises=[], show_subformulas=True,parentheses=Fa
   for p in premises:
     df_true = df_true[df_true[p.toString(parentheses=parentheses)]==1]
   for index, row in df_true.iterrows():
+    print(row[formula.toString(parentheses=parentheses)])
     if row[formula.toString(parentheses=parentheses)]==0:
       return index
   return None
