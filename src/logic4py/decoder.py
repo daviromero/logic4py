76eb0816455bf9f830c7059a8789e001237f5bd8
Davi Romero de Vasconcelos <daviromero@ufc.br>
2023-02-15 18:28:04 -0300

    novo método para contraexemplo

--- src/logic4py/decoder.py
+++ src/logic4py/decoder.py
@@ -1,4 +1,6 @@
 import ruamel.yaml
+
+import ruamel.yaml
 def decode_fo_interpretation(input_fo_interpretatio, universe_key='U'):
   data_fo_interpretatio = input_fo_interpretatio.replace('=',':').replace('{','[').replace('}',']').replace('set()','[]').replace('(','[').replace(')',']')
   yaml = ruamel.yaml.YAML(typ='safe')
@@ -27,13 +29,15 @@ def decode_fo_interpretation(input_fo_interpretatio, universe_key='U'):
             raise ValueError(f'No predicado {key}, a tupla {tuple(t)} deve ter tamanho {arity}')
           for k in t:
             if not k in U:
-              raise ValueError(f'No predicado {key}, o elemento {k} da tupla {t} deveria fazer parte do conjunto universo')
+              raise ValueError(f'No predicado {key}, o elemento {k} da tupla {tuple(t)} deveria fazer parte do conjunto universo')
       elif arity==1:
         for t in data[key]:
-          if type(t)==list:
-            raise ValueError(f'No predicado {key}, {t} deveria ser uma tupla de tamanho {arity}')
-          if not t in U:
+          if type(t)==list and len(t)!=arity:
+            raise ValueError(f'No predicado {key}, {tuple(t)} deveria ser uma tupla de tamanho {arity}')
+          elif type(t)!=list and not t in U:
             raise ValueError(f'No predicado {key}, {t} deveria fazer parte do conjunto universo')
+          elif type(t)==list and not t[0] in U:
+            raise ValueError(f'No predicado {key}, {t[0]} deveria fazer parte do conjunto universo')
       preds[s_key,str(arity)] = set()
       for t in data[key]:
         if (type(t)!=list):
